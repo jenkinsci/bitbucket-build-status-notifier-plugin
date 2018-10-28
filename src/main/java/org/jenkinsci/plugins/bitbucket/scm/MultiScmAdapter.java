abb6b828e45c878bfad88dc71a5cb0e9f06827d4
René Scheibe <rene.scheibe@gmail.com>
2018-10-28 16:47:35 +0100

    Simplify code using a foreach loop

--- src/main/java/org/jenkinsci/plugins/bitbucket/scm/MultiScmAdapter.java
+++ src/main/java/org/jenkinsci/plugins/bitbucket/scm/MultiScmAdapter.java
@@ -34,8 +34,6 @@ import org.eclipse.jgit.transport.URIish;
 import org.jenkinsci.plugins.multiplescms.MultiSCM;
 
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 public class MultiScmAdapter implements ScmAdapter {
@@ -50,10 +48,8 @@ public class MultiScmAdapter implements ScmAdapter {
 
     public Map getCommitRepoMap() throws Exception {
         HashMap<String, URIish> commitRepoMap = new HashMap<String, URIish>();
-        List<SCM> scms = multiScm.getConfiguredSCMs();
 
-        for (Iterator<SCM> i = scms.iterator(); i.hasNext(); ) {
-            SCM scm = i.next();
+        for (SCM scm : multiScm.getConfiguredSCMs()) {
             if (scm instanceof GitSCM) {
                 commitRepoMap.putAll(new GitScmAdapter((GitSCM) scm, this.build).getCommitRepoMap());
             } else if (scm instanceof MercurialSCM) {
